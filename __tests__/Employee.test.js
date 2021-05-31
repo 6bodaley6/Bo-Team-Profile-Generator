9b715b74908a6d337da62771a63bf0a69a5284f4
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-05-31 04:06:42 -0600

    added style to card

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,9 @@
- 
+const Employee = require("../lib/Employee");
+
+test("creates a object", () => {
+    const employee = new Employee("name", 1, "email", "github");
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    // expect(employee.github).toEqual(expect.any(String));
+});
